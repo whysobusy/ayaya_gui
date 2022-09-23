7b71dc851eec98d00e16cdbe58a1dc838603157b
whysobusy <penetrate.darkness@gmail.com>
2022-09-23 16:40:56 +0800

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+#ayayaya
+yayaaya
---
+++ ayaya_gui/ayaya_gui.cpp
@@ -0,0 +1,12 @@
+﻿// ayaya_gui.cpp : Defines the entry point for the application.
+//
+
+#include "ayaya_gui.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
